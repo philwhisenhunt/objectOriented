21a4cbd4d8f2ba6ff8fe5a25f7dcc74505a2b0da
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-04 17:24:59 -0500

    Checking in progress for evening

--- Person.php
+++ Person.php
@@ -24,24 +24,39 @@ class Business {
         $this->staff->add($person);
 
     }
+
+    public function getStaffMembers()
+    {
+        return $this->staff->members();
+    }
 }
 
 class Staff {
     protected $members = [];
+
+    public function __construct($members = [])
+    {
+        $this->members = $members;
+    }
 
     public function add(Person $person)
     {
         $this->members[] = $person;
     }
 
+    public function members()
+    {
+        return $this->members;
+    }
+
 }
 
 $jeffrey = new Person('Jeffrey Way'); 
 
-$staff = new Staff;
+$staff = new Staff([$jeffrey]);
 
 $laracasts = new Business($staff);
 
-$laracasts->hire($jeffrey);
+$laracasts->hire(new Person('Jane Doe'));
 
-var_dump($staff);
+var_dump($laracasts->getStaffMembers());
