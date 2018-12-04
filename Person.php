8be86f2abf1401ddd7ef2db92b3fac970e529c24
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-04 10:55:02 -0500

    Adding set method

---
+++ Person.php
@@ -0,0 +1,29 @@
+<?php
+
+class Person{
+    public $name;
+
+    public $age;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setAge($age)
+    {
+        if($age < 18)   
+        {
+            throw new Exception("Person is not old enough.");
+        }
+        
+        $this->age = $age;
+    }
+
+}
+
+$john = new Person('John Doe');
+$john->setAge(17);
+
+
+var_dump($john);
