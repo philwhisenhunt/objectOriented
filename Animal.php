98350c671b4f830be5f92269edb58a172be045c0
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-05 12:18:28 -0500

    added animal examples for interfaces

--- Animal.php
+++ Animal.php
@@ -1,5 +1,18 @@
 <?php
 
 interface animal {
-    
+    public function communicate();
+
+}
+
+class Dog implements Animal {
+    public function communicate(){
+        return 'bark';
+    }
+}
+
+class Cat implements Animal {
+    public function communicate(){
+        return 'meow';
+    }
 }
