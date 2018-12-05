d3eb85e12540c69efaec8a328afce28ee8b855d7
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-05 13:55:44 -0500

    checking in interface progress

--- Logger.php
+++ Logger.php
@@ -1,14 +1,17 @@
 <?php
-interface Logger {}
+interface Logger {
+    public function execute($message);
+   
+}
 
-class LogToFile {
+class LogToFile implements Logger{
     public function execute($message)
     {
-        var_dump('log the message to a file');
+        var_dump('log the message to a file: ' . $message);
     }
 }
 
-class LogToDatabase {
+class LogToDatabase implements Logger{
     public function execute($message)
     {
         var_dump('log the message to a database');
@@ -19,17 +22,19 @@ class LogToDatabase {
 class UsersController {
 
     protected $logger;
-    public function __construct(LogToFile $logger)
+    public function __construct(Logger $logger)
     {
-
+        $this->logger = $logger;
     }
     public function show()
     {
         $user = 'JohnDoe';
 
-        //log this information 
         $this->logger->execute($user);
     }
 }
 
 $controller = new UsersController(new LogToFile);
+
+//Send a message to the logger. 
+$controller->show();
